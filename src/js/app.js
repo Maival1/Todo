e7d1a666ac0dd14622d28623fb88f04855087f7c
@Maival1 <maival.cimba@gmail.com>
2023-05-02 01:06:20 +0300

    feat(todo) made moving desk

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,11 @@ import * as bootstrap from 'bootstrap'
 // Variables
 
 let data = []
-const listContentElement = document.querySelector('#listContent')
+let dataProggres = []
+let dataDone = []
+const listContentTodoElement = document.querySelector('#listContentTodo')
+const listContentProggresElement = document.querySelector('#listContentProggres')
+const listContentDoneElement = document.querySelector('#listContentDone')
 const listAddElemnt = document.querySelector('#listAdd')
 const formElement = document.querySelector('#form')
 const inputListElement = document.querySelector('#inputList')
@@ -12,16 +16,22 @@ const textareaListElement = document.querySelector('#textareaList')
 const userElement = document.querySelector('#user')
 const bgColorElement = document.querySelector('#bgColor')
 const buttonConfirmElement = document.querySelector('#confirmList')
+
+
+
 // Listeners
 
 
-// formElement.addEventListener('submit', handleSubmitForm)
-buttonConfirmElement.addEventListener('click', handleSubmitForm)
+formElement.addEventListener('submit', handleSubmitForm)
+
+listContentTodoElement.addEventListener('click', hendleDropDeskTodo)
+listContentProggresElement.addEventListener('click', hendleDropDeskProggres)
+listContentDoneElement.addEventListener('click', hendleDropDeskDone)
 
 // Hendlers
 
 function handleSubmitForm (event) {
-  // event.preventDefault()
+  event.preventDefault()
 
   const title = inputListElement.value
   const description = textareaListElement.value
@@ -32,8 +42,86 @@ function handleSubmitForm (event) {
   data.push(todo)
   console.log(data)
 
-  render(data, listContentElement)
+  render(data, listContentTodoElement)
   // formElement.reset()
+}
+
+function hendleDropDeskTodo (event) {
+
+  const button = event.target.closest('.dropdown-item')
+  if (button) {
+    const value = button.value
+    const desk = button.closest('.desk')
+    const deskID = desk.id
+    if(value == 'progress') {
+      dataProggres.push(data.find((item) => item.id == `${deskID}`))
+      data.forEach(function(item, i) {
+        if (item.id == `${deskID}`) data.splice(i, 1)
+      })
+      render(dataProggres, listContentProggresElement)
+      render(data, listContentTodoElement)
+    }
+    if(value == 'done') {
+      dataDone.push(data.find((item) => item.id == `${deskID}`))
+      data.forEach(function(item, i) {
+        if (item.id == `${deskID}`) data.splice(i, 1)
+      })
+      render(dataDone, listContentDoneElement)
+      render(data, listContentTodoElement)
+    }
+  }
+}
+
+function hendleDropDeskProggres (event) {
+
+  const button = event.target.closest('.dropdown-item')
+  if (button) {
+    const value = button.value
+    const desk = button.closest('.desk')
+    const deskID = desk.id
+    if(value == 'todo') {
+      data.push(dataProggres.find((item) => item.id == `${deskID}`))
+      dataProggres.forEach(function(item, i) {
+        if (item.id == `${deskID}`) dataProggres.splice(i, 1)
+      })
+      render(dataProggres, listContentProggresElement)
+      render(data, listContentTodoElement)
+    }
+    if(value == 'done') {
+      dataDone.push(dataProggres.find((item) => item.id == `${deskID}`))
+      dataProggres.forEach(function(item, i) {
+        if (item.id == `${deskID}`) dataProggres.splice(i, 1)
+      })
+      render(dataDone, listContentDoneElement)
+      render(dataProggres, listContentProggresElement)
+    }
+  }
+}
+
+function hendleDropDeskDone (event) {
+
+  const button = event.target.closest('.dropdown-item')
+  if (button) {
+    const value = button.value
+    const desk = button.closest('.desk')
+    const deskID = desk.id
+    if(value == 'progress') {
+      dataProggres.push(dataDone.find((item) => item.id == `${deskID}`))
+      dataDone.forEach(function(item, i) {
+        if (item.id == `${deskID}`) dataDone.splice(i, 1)
+      })
+      render(dataProggres, listContentProggresElement)
+      render(dataDone, listContentDoneElement)
+    }
+    if(value == 'todo') {
+      data.push(dataDone.find((item) => item.id == `${deskID}`))
+      dataDone.forEach(function(item, i) {
+        if (item.id == `${deskID}`) dataDone.splice(i, 1)
+      })
+      render(dataDone, listContentDoneElement)
+      render(data, listContentTodoElement)
+    }
+  }
 }
 
 // Constructors
@@ -51,13 +139,13 @@ function Desk (title, description, user, bgColor) {
 // Templates
 
 function buildDeskTemplate (data) {
-  const date = new Date(data.date).toLocaleString()
+  const time = new Date(data.date).toLocaleString()
 
   return `
-  <div class="desk ${data.bgColor}">
+  <div id="${data.id}" class="desk ${data.bgColor}">
   <div class="desk__head d-flex">
     <div class="desk__title">${data.title}</div>
-    <time class="desk__time">${date}</time>
+    <time class="desk__time">${time}</time>
   </div>
   <p class="desk__text">${data.description}</p>
   <div class="desk__user">${data.user}<img></div>
@@ -67,10 +155,9 @@ function buildDeskTemplate (data) {
         Dropdown button
       </button>
       <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">todo</a></li>
-        <li><a class="dropdown-item" href="#">in progress</a></li>
-        <li><a class="dropdown-item" href="#">done</a></li>
-      </ul>
+        <li><button class="dropdown-item todo-btn" value="todo"  >todo</button></li>
+        <li><button class="dropdown-item progress-btn" value="progress">in progress</button></li>
+        <li><button class="dropdown-item done-btn" value="done">done</button></li>
     </div>
     <button class="btn btn-primary">Edit</button>
     <button class="btn btn-danger">Remove</button>
@@ -91,23 +178,3 @@ function render(collection, wrapper) {
   wrapper.innerHTML = templates
 }
 
-// Modal
-
-// const exampleModal = document.getElementById('exampleModal')
-// if (exampleModal) {
-//   exampleModal.addEventListener('show.bs.modal', event => {
-//     // Button that triggered the modal
-//     const button = event.relatedTarget
-//     // Extract info from data-bs-* attributes
-//     const recipient = button.getAttribute('data-bs-whatever')
-//     // If necessary, you could initiate an Ajax request here
-//     // and then do the updating in a callback.
-
-//     // Update the modal's content.
-//     const modalTitle = exampleModal.querySelector('.modal-title')
-//     const modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-//     modalTitle.textContent = `New message to ${recipient}`
-//     modalBodyInput.value = recipient
-//   })
-// }
